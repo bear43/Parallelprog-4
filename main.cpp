d83a1a0e218acac0f4ed1114beac262b96787a92
bear43 <bigwilly4307@gmail.com>
2019-03-16 14:33:13 +0400

    Changed timing logic. Now last endtime go in g_cBuffer then value from g_cBuffer are copied in starttime variable but endtime received by MPI_Wtime(). This approach goes to right time calculation. Total work time calculation is as before

--- main.cpp
+++ main.cpp
@@ -31,40 +31,45 @@ int main(int argc, char **argv)
     g_cBuffer = new char[g_iBufferSize];//Allocate memory for buffer
     if(g_iProcessRank == 0)//Is this root process?
     {
-        printf("Using buffer with its size %i\n", g_iBufferSize);
-        memset(g_cBuffer, 0, (size_t)g_iBufferSize);//Fill memory with zero byte
-        if(nextProcessNumber >= g_iTotalProcessCount)
+        if(nextProcessNumber < g_iTotalProcessCount)
+        {
+
+            printf("Using buffer with its size %i\n", g_iBufferSize);
+            volatile double starttime = MPI_Wtime();
+            memcpy(g_cBuffer, (const void*)&starttime, sizeof(double));
+            MPI_Send(g_cBuffer, g_iBufferSize, MPI_CHAR, nextProcessNumber, 0, MPI_COMM_WORLD);
+            //g_cBuffer - destination point to set memory values
+            //g_iBufferSize - buffer size. Like Array.Count in C# or Array.length in Java
+            MPI_Status status;
+            memset(&status, 0, sizeof(MPI_Status));//Fill memory with zero byte
+            prevProcessNumber = g_iTotalProcessCount - 1;
+            MPI_Recv(g_cBuffer, g_iBufferSize, MPI_CHAR, prevProcessNumber, 0, MPI_COMM_WORLD, &status);
+            status.MPI_ERROR != 0 ? printf("[Root process] Catch error code by MPI_Recv: %i\n", status.MPI_ERROR)
+                                  : printf("[Root process] Successful received %i bytes\n", g_iBufferSize);
+            fflush(stdout);
+            volatile double endtime = MPI_Wtime();
+            printf("[Root process] Total work time: %lf\n", endtime - starttime);
+            fflush(stdout);
+        }
+        else
         {
             printf("This process are single process in that running configuration. I can not send buffer to nowhere!\n");
-            return 0;
         }
-        double time = MPI_Wtime();
-        MPI_Send(g_cBuffer, g_iBufferSize, MPI_CHAR, nextProcessNumber, 0, MPI_COMM_WORLD);
-        //g_cBuffer - destination point to set memory values
-        //g_iBufferSize - buffer size. Like Array.Count in C# or Array.length in Java
-        MPI_Status status;
-        memset(&status, 0, sizeof(MPI_Status));//Fill memory with zero byte
-        prevProcessNumber = g_iTotalProcessCount-1;
-        MPI_Recv(g_cBuffer, g_iBufferSize, MPI_CHAR, prevProcessNumber, 0, MPI_COMM_WORLD, &status);
-        status.MPI_ERROR != 0 ? printf("[Root process] Catch error code by MPI_Recv: %i\n", status.MPI_ERROR)
-                              : printf("[Root process] Successful received %i bytes\n", g_iBufferSize);
-        fflush(stdout);
-        time = MPI_Wtime()-time;
-        printf("[Root process] Total work time: %lf\n", time);
-        fflush(stdout);
     }
     else//This is not root process
     {
         MPI_Status status;
         memset(&status, 0, sizeof(MPI_Status));//Fill memory with zero byte
-        double time = MPI_Wtime();
+        volatile double starttime;
         MPI_Recv(g_cBuffer, g_iBufferSize, MPI_CHAR, prevProcessNumber, 0, MPI_COMM_WORLD, &status);
-        time = MPI_Wtime() - time;
+        memcpy((void*)&starttime, (const void*)g_cBuffer, sizeof(double));
+        volatile double endtime = MPI_Wtime();
         status.MPI_ERROR != 0 ? printf("[Process %i] Catch error code by MPI_Recv: %i\n", g_iProcessRank+1, status.MPI_ERROR)
-                              : printf("[Process %i] Successful received %i bytes in %lf seconds\n", g_iProcessRank+1, g_iBufferSize, time);
+                              : printf("[Process %i] Successful received %i bytes in %lf seconds\n", g_iProcessRank+1, g_iBufferSize, endtime - starttime);
         fflush(stdout);
         if(nextProcessNumber == g_iTotalProcessCount)
             nextProcessNumber = 0;
+        memcpy(g_cBuffer, (const void*)&endtime, sizeof(double));
         MPI_Send(g_cBuffer, g_iBufferSize, MPI_CHAR, nextProcessNumber, 0, MPI_COMM_WORLD);
     }
     delete[] g_cBuffer;//Clean memory
